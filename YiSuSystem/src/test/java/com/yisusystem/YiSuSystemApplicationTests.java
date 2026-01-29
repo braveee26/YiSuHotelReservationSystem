06d8a91a85cc91ba4cc8ad6d5cfbe1d1a082e470
刘馥铭 <2023213418@mail.hfut.edu.cn>
2026-01-29 23:04:44 +0800

    前后端架子大概搭出来，主要是前端的路由，做出登录注册伪数据，后端需要先设计数据库的表再搞。

---
+++ YiSuSystem/src/test/java/com/yisusystem/YiSuSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yisusystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YiSuSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
